172a7fa5e11a9a726ba0b98caff56a2d6ae7896b
Piotr <piotradamczyk2111@gmail.com>
2020-05-23 15:33:39 +0200

    finish

--- src/main/java/TennisGame.java
+++ src/main/java/TennisGame.java
@@ -12,22 +12,22 @@ public class TennisGame {
 
     public String getScore() {
 
-//		 int ordinal = pointType.ordinal();
-//		 pointType = PointType.values()[score + 1];
-
-		 if (getPlayerTwo().score() - getPlayerOne().score() >= 2) {
-			 return "P2 won";
-		 } else if (getPlayerTwo().score() - getPlayerOne().score() <= -2) {
-			 return "P1 won";
-		 } else if (getPlayerTwo().score().equals(PointType.ADVANTAGE.ordinal())) {
-			 return "P2 advantage";
-		 } else if (getPlayerOne().score().equals(PointType.ADVANTAGE.ordinal())) {
-			 return "P1 advantage";
-		 } else if (PointType.FORTY.equals(getPlayerOne().score()) && getPlayerTwo().score().equals(getPlayerOne().score())) {
-			 return "deuce";
-		 } else if (getPlayerTwo().score().equals(getPlayerOne().score())) {
-			 return PointType.getByOrdinal(getPlayerTwo().score()) + " all";
-		 }
+        if (getPlayerTwo().score >= PointType.values().length && getPlayerOne().score >= PointType.values().length) {
+            return "deuce";
+        }
+        if (getPlayerTwo().score() - getPlayerOne().score() >= 2) {
+            return "P2 won";
+        } else if (getPlayerTwo().score() - getPlayerOne().score() <= -2) {
+            return "P1 won";
+        } else if (getPlayerTwo().score().equals(PointType.ADVANTAGE.ordinal())) {
+            return "P2 advantage";
+        } else if (getPlayerOne().score().equals(PointType.ADVANTAGE.ordinal())) {
+            return "P1 advantage";
+        } else if (PointType.FORTY.getTranslated().equals(PointType.getByOrdinal(getPlayerOne().score())) && getPlayerTwo().score().equals(getPlayerOne().score())) {
+            return "deuce";
+        } else if (getPlayerTwo().score().equals(getPlayerOne().score())) {
+            return PointType.getByOrdinal(getPlayerTwo().score()) + " all";
+        }
 
         return PointType.getByOrdinal(getPlayerTwo().score()) + " - " + PointType.getByOrdinal(getPlayerTwo().score());
     }
